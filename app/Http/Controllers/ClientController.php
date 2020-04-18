7374090c2fff35329256acfc6828799768c06ec1
Ruan Haarhoff <ruan.haarhoff@gmail.com>
2020-04-18 15:47:46 +0200

    workflow-example: Added controllers and api endpoints

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+/**
+ * @author Ruan Haarhoff <ruan@aptic.com>
+ * @since 20200418 Initial creation.
+ */
+class ClientController extends Controller
+{
+    /**
+     * @return Response
+     */
+    public function index(): Response
+    {
+        //
+    }
+
+    /**
+     * @return Response
+     */
+    public function create(): Response
+    {
+        //
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function store(Request $request): Response
+    {
+        //
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function show($id): Response
+    {
+        //
+    }
+
+    /**
+     * @param $id
+     *
+     * @return Response
+     */
+    public function edit($id): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id): Response
+    {
+        //
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function destroy($id): Response
+    {
+        //
+    }
+}
